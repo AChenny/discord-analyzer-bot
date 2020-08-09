e94122f270882e735d4ebc5f5959b009af73cfd3
Anderson Chen <anderson_chen97@hotmail.com>
2020-08-09 01:45:01 -0700

    Added create_server_query

--- queryHelper.js
+++ queryHelper.js
@@ -4,9 +4,10 @@
 const DATABASE_NAME = 'beta_bois';
 
 // Table names
-const AUTHOR_TABLE_NAME = 'authors'
+const AUTHOR_TABLE_NAME = 'authors';
+const SERVER_TABLE_NAME = 'servers';
 
-// Description: Creates the author table query from authors object 
+// Description: Creates the author table query
 // Parameters:
 //      id (String) *Required
 //          The ID of the author. Will be used as the primary key in this database
@@ -16,6 +17,9 @@ const AUTHOR_TABLE_NAME = 'authors'
 //          Numbers at the end of
 //      avatarUrl (String)
 //          Link to the URL of the author
+// Returns:
+//      query (String)
+//          The query string to be sent to the database
 function create_author_query (id, username, discriminator, avatarUrl) {
     let parameters = {
         'id': id, 
@@ -30,7 +34,6 @@ function create_author_query (id, username, discriminator, avatarUrl) {
     }
 
     let columnNameString = Object.keys(parameters).join(', ');
-    
     let valueStrings = Object.values(parameters).join(', ');
     
     let query = `INSERT IGNORE INTO ${AUTHOR_TABLE_NAME} (${columnNameString}) VALUES (${valueStrings});`;
@@ -38,7 +41,30 @@ function create_author_query (id, username, discriminator, avatarUrl) {
     return query;
 }
 
+
+// Description: Creates a server table query
+// Parameters:
+//      serverId (String) *Required
+//          The ID of the server. Will be used as the primary key in this database
+//      serverName (String) *Required
+//          The name of the server
+// Returns:
+//      query (String)
+//          The query string to be sent to the database
+function create_server_query (serverId, serverName) {
+    let parameters = {
+        'id' : serverId,
+        'name' : `"${serverName}"`
+    };
+
+    let columnNameString = Object.keys(parameters).join(', ');
+    let valueStrings = Object.values(parameters).join(', ');
+
+    let query = `INSERT IGNORE INTO ${SERVER_TABLE_NAME} (${columnNameString}) VALUES (${valueStrings});`;
+    return query;
+}
+
 // Export the functions
 module.exports = {
-    create_author_query
+    create_author_query, create_server_query
 }
