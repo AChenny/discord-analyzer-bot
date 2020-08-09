45cef53d61ce7f563bc56371b195563629c4194c
Anderson Chen <anderson_chen97@hotmail.com>
2020-08-09 03:02:05 -0700

    Updated upload_file to return object url
    + Also updated the header for upload_file to be more verbose

--- s3BucketHelper.js
+++ s3BucketHelper.js
@@ -1,6 +1,5 @@
 // Description: All functions to handle s3 bucket interactions
 const config = require('./config.json');
-const path = require('path');
 
 const AWS_ACCESS_KEY = config.AWS_CREDENTIALS.access_key;
 const AWS_SECRET_KEY = config.AWS_CREDENTIALS.secret_key;
@@ -14,26 +13,41 @@ const s3 = new AWS.S3({
 const BUCKET_NAME = 'discord-bot-bucket';
 
 // Description: Uploads a file to s3 bucket in a date folder -> user
-// Input: Data from file to upload (bytestream), filename including the extension(string), username to make the folder for(string) 
-// Output: None
-function upload_file (data, filename, username) {
-    let bucketKey = [getCurrentDateFolderName(), username, filename].join('/'); 
-    
+// Parameters: 
+//      data (bytestream/bytestring)
+//          The stream of data in byte form to upload to the bucket
+//      filename (String)
+//          The filename to be used in the bucket including the extension
+//              Example: 'picture.png'
+//      username (String) 
+//          Name of the username which will be used as the name of the folder
+// Returns:
+//      object_url (String)
+//          The url to the object in the bucket or null if the upload failed
+async function upload_file(data, filename, username) {
+    let bucketKey = [getCurrentDateFolderName(), username, filename].join('/');
     const params = {
         Bucket: BUCKET_NAME,
         Key: bucketKey,
         Body: data
     };
-    s3.upload(params, function(err, data) {
-        console.log(err, data);
-    });
-    return;
+
+    let object_url;
+    try {
+        const data = await s3.upload(params).promise();
+        object_url = data.Location;
+    }
+    catch (err) {
+        throw err;
+    }
+    return object_url;
 }
 
 function getCurrentDateFolderName() {
     let d = new Date();
-    return d.toLocaleDateString('default', { month: 'long', year: 'numeric'});
+    return d.toLocaleDateString('default', { month: 'long', year: 'numeric' });
 }
+
 
 module.exports = {
     upload_file
