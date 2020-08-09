cc9defecd18f1471dbec1390670833140b6c6fe3
Anderson Chen <anderson_chen97@hotmail.com>
2020-08-08 17:40:43 -0700

    Added transaction function as multiple queries
    + Added send_queries_to_db_in_transaction which will take a list of
    queries and a database name to send an array of queries together and
    rollback if any of them fail. Also will log any errors

--- dbHelper.js
+++ dbHelper.js
@@ -2,7 +2,6 @@
 
 // Imports
 const config = require('./config.json');
-// const mysql = require('mysql2'); //https://www.npmjs.com/package/mysql2
 const mysql = require('mysql2/promise');
 // Constants and configuration
 
@@ -30,7 +29,70 @@ async function query_db(query, database_name) {
     return;
 }
 
+// Description: Begins a transaction, sends the list of queries from arrays and commits or rollsback if there is an error
+//  Parameters:
+//      queries (Array of strings)
+//          The queries to be sent to the database
+//      database_name (String)
+//          The name of the database these queries will be sent to
+//  Returns:
+//      None
+async function send_queries_to_db_in_transaction(queries, database_name) {
+    const connection = await mysql.createConnection(
+        {
+            host: config.database_creds.host, 
+            user: config.database_creds.user, 
+            password: config.database_creds.password,
+            database: database_name
+        }
+    );
+
+    // begin transaction
+    console.log("Beginning transaction...");
+    await connection.beginTransaction();
+
+    // Send queries
+    //      If, there are any errors, then rollback the transaction
+    //      Else, commit
+    const start_queries = async () => {
+        let query_success = true;
+        await asyncForEach(queries, async (query) => {
+            console.log(`Sending query: ${query}`);
+            try {
+                await connection.query(query);
+            }
+            catch(err) {
+                console.log("Rolling back...");
+                connection.rollback();
+                console.log(err);
+                // If any of the queries has an error, set flag to false and 
+                query_success = false;
+            }
+        })
+        if (query_success) {
+            console.log("Committing...");
+            let commit_response = await connection.commit();
+            console.log("Commit Success!")
+        }
+    }
+    start_queries();
+    return;
+}
+
+// Description: Asynchrounous foreach function, works the same way as foreach, but async.
+// Input:
+//      array (Array)
+//          Some array to loop through
+//      callback (function)
+//          Some async function to call on the array
+// Output: None
+async function asyncForEach(array, callback) {
+    for (let index = 0; index < array.length; index++) {
+      await callback(array[index], index, array);
+    }
+}
+
 // Export the functions
 module.exports = {
-    query_db
+    query_db, send_queries_to_db_in_transaction
 }
